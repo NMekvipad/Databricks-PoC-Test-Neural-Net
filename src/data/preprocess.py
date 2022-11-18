55c3be7c2146e9d6768321b60f307e90d66645b6
Nuttapong Mekvipad <n.mekvipad@hotmail.com>
2022-11-18 14:24:54 +0100

    Add dataloader

---
+++ src/data/preprocess.py
@@ -0,0 +1,17 @@
+# Test feature store capability by implementing 2 different data transformation strategy
+# and 2 different action transformation
+
+# Data transformation strategy
+#   1. 3-Months running average
+#   2. Transform column 1-5 by adding them together and use new column instead of those 5 column
+#   and transform column 6-10 by average them together and add it as a new column
+
+# Action transformation
+#   1. Min-max normalized
+#   2. Standardizer
+
+#############################################################################################
+#                       Use running average for all feature columns                         #
+#############################################################################################
+
+
